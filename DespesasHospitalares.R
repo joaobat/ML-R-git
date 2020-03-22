9d9f128593de51e3ac44e506eac17eeaa1c8e33c
Joao Souza <desenvolvedorfp@gmail.com>
2020-03-22 16:07:33 -0300

    Criação do modelo de regressao versão 1

--- DespesasHospitalares.R
+++ DespesasHospitalares.R
@@ -15,4 +15,59 @@ str(despesas)
 summary(despesas)
 
 # Construindo um histograma
-hist(despesas$gastos, main = "Gastos com pacientes em (R$)", xlab = "Gastos (R$)")
+hist(despesas$gastos, main = "Gastos com despesas hospitalares (R$)", xlab = "Gastos (R$)")
+
+# Gerando uma tabela de contingencia das regiões
+table(despesas$regiao)
+
+# Matriz de correlação -> relação entre as variaveis
+cor(despesas[c("idade", "bmi", "filhos", "gastos")])
+
+# Nenhuma das correlações na matriz é considerada forte, mas existem algumas associaçõe interessantes.
+# A idade e o bmi (IMC) parecem ter uma correlação positiva fraca, o que significa que 
+# com o aumento da idade, a massa corporal tende a aumentar. há uma correlação positiva 
+# moderada entre a idade e os gasto, além do numero de filhos e os gastos.
+# Estas associações implicam que a medida que idade, massa corporal e numeros de filhos aumenta,
+# o custo esperado do seguro saude sobe.
+
+# Visualizando relacionamento entre as variaveis: scatterplot
+pairs(despesas[c("idade", "bmi", "filhos", "gastos")])
+
+# Scatterplot Matrix
+install.packages("psych")
+library(psych)
+
+# Obtendo mais informações sobre o relacionamento entre as variaveis
+pairs.panels(despesas[c("idade", "bmi", "filhos", "gastos")])
+
+# Construir o modelo - usando a função lm()
+# Usamos para este problema o modelo de regressão linear multipla
+modelo <- lm(gastos ~ ., data = despesas)
+
+# Visualizando os coeficientes
+modelo
+
+# Prevendo despesas médicas
+despesasteste <- read.csv("despesas-teste.csv")
+View(despesasteste)
+previsao <- predict(modelo, despesasteste)
+View(previsao)
+
+# Avaliando a performance do modelo
+summary(modelo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
